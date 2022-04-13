8c12c3d308dff9589efc4070657e7d84c06525ad
mansamusah254 <mansamusah830@gmail.com>
2022-04-13 12:32:13 +0300

    confirmed working highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,6 +6,6 @@ import { Directive, ElementRef } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private elem:ElementRef) {
-    elem.nativeElement.style.backgroundColor = 'yellow'
+    elem.nativeElement.style.backgroundColor = 'grey'
    }
 }
